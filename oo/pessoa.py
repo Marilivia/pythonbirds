a4b57185a772e1fc6cb53d275b19c7b9682e8818
marilivia <mariliviamelo@hotmail.com>
2019-04-16 07:23:11 -0300

    criando atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,27 @@
 class Pessoa:
+    olhos = 2 #atributo de classe
+    def __init__(self, *filhos, nome=None,idade = 0):
+        #atributos de instancia
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
+
     def cumprimentar (self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print (id(p))
-    print(p.cumprimentar())
+    renzo = Pessoa (nome = 'Renzo',idade=40)
+    carlos = Pessoa(nome='Carlos',idade=23)
+    luciano = Pessoa(renzo ,carlos,nome='Luciano')
+    for filho in luciano.filhos:
+        print(filho.nome, filho.idade)
+
+
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.nome)
+    print(luciano.idade)
+    print(luciano.filhos)
+    print(Pessoa.olhos)
+    print(luciano.olhos)
 
